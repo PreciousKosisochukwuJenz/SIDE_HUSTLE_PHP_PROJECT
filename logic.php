f49e76f1bffdc6800542f0da6ce7fc9fb8937ef5
=Precious Kosisochukwu <=preciouskosisochukwu01@gmail.com>
2021-06-22 14:18:08 +0100

    Uploading my task for week one

---
+++ logic.php
@@ -0,0 +1,25 @@
+<?php
+
+    #A method for printing numbers within the range of start input and end input
+    function GetRangeOfNumbers($start, $end){
+        $arr = array();
+        $counter = 0;
+        echo("Below are numbers within the range of $start to $end <hr>");
+        for($i = $start; $i <= $end; $i++){
+            $arr[$counter] = $i;
+            echo($arr[$counter]);
+            echo("<br>");
+            $counter++;
+        }
+    }
+
+    #A method for printing the sum of numbers in an array
+    function SumArray($arr){
+        $arrCount = count($arr);
+        $sum = 0;
+        for($i = 0; $i < $arrCount; $i++){
+            $sum += $arr[$i];
+        }
+        echo("The sum of the numbers in the array is: $sum");
+    }
+?>
